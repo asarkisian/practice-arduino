120180eb673f1d219391179fb06d5b4e8eb3dc97
Armond Sarkisian <armond.sarkisian@gmail.com>
2023-10-09 18:15:40 -0700

    updated

--- led_builtin_delay/sketch_led_builtin_delay/sketch_led_builtin_delay.ino
+++ led_builtin_delay/sketch_led_builtin_delay/sketch_led_builtin_delay.ino
@@ -1,10 +1,13 @@
+#define TIMER_ON 500
+#define TIMER_OFF 500
+
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop() {
   digitalWrite(LED_BUILTIN, HIGH);
-  delay(1000);
+  delay(TIMER_ON);
   digitalWrite(LED_BUILTIN, LOW);
-  delay(1000);
+  delay(TIMER_OFF);
 }
