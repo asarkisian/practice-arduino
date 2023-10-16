0c2d60fabe4d735462f2d3697658c903dd014361
Armond Sarkisian <armond.sarkisian@gmail.com>
2023-10-15 17:58:24 -0700

    updated

---
+++ max_7219_dot_matrix_paint_dots/max_7219_dot_matrix_paint_dots.ino
@@ -0,0 +1,81 @@
+#include <LedControl.h>
+
+using namespace std;
+
+#define JOYSTICK1_X A0
+#define JOYSTICK1_Y A1
+
+LedControl lc = LedControl(12, 10, 11, 1);
+
+short dot1[2] = {
+  0,
+  0
+};
+
+void setup() {
+  // initialize serial communications
+  Serial.begin(9600);
+
+  // initialize the led dot matrix
+  lc.shutdown(0, false);
+  lc.setIntensity(0, 8);
+  lc.clearDisplay(0);
+
+  // assign joystick pins
+  pinMode(JOYSTICK1_X, INPUT);
+  pinMode(JOYSTICK1_Y, INPUT);
+
+  // initial draw of dot
+  lc.setLed(0, dot1[0], dot1[1], true);
+}
+
+void paintScreen() {
+    lc.setLed(0, dot1[0], dot1[1], true);
+}
+
+void drawDot1(const short row, const short col) {
+  bool isUpdateScreen = false;
+
+  if (row >= 0 && row <= 7) {
+    isUpdateScreen = true;
+    dot1[0] = row;
+  }
+
+  if (col >= 0 && col <= 7) {
+    isUpdateScreen = true;
+    dot1[1] = col;
+  }
+
+  if (isUpdateScreen) {
+    paintScreen();
+  }
+}
+
+void checkIfClear() {
+  if (dot1[0] == 0 && dot1[1] == 0) {
+    lc.clearDisplay(0);
+  }
+}
+
+void readJoystick1() {
+  const int readJoystick1X = analogRead(JOYSTICK1_X);
+  const int readJoystick1Y = analogRead(JOYSTICK1_Y);
+
+  if (readJoystick1X > 530) {
+    drawDot1(dot1[0], dot1[1]+1);
+  } else if (readJoystick1X < 515) {
+    drawDot1(dot1[0], dot1[1]-1);
+  }
+
+  if (readJoystick1Y > 530) {
+    drawDot1(dot1[0]+1, dot1[1]);
+  } else if (readJoystick1Y < 515) {
+    drawDot1(dot1[0]-1, dot1[1]);
+  }
+}
+
+void loop() {
+  readJoystick1();
+  checkIfClear();
+  delay(100);
+}
