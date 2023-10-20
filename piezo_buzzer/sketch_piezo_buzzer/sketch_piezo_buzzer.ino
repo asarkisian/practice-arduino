7adb6c15e7aeb507d6a8987668901f3d1fca12be
Armond Sarkisian <armond.sarkisian@gmail.com>
2023-10-19 19:12:04 -0700

    updated

--- piezo_buzzer/sketch_piezo_buzzer/sketch_piezo_buzzer.ino
+++ piezo_buzzer/sketch_piezo_buzzer/sketch_piezo_buzzer.ino
@@ -5,7 +5,7 @@
 // Note:"31 hertz is the lowest frequency
 // Note: must call noTone() if using multiple piezo buzzers on separate pins
 
-const short PIEZO_PIN = 8
+const short PIEZO_PIN = 8;
 
 void setup() {
   // no setup required for piezo buzzer
@@ -15,7 +15,9 @@ void loop() {
   // Tone needs 2 arguments to work, but can take three
   // 1) Pin #
   // 2) Frequency - this is in hertz (cycles per second) which determines the pitch
-  tone(PIEZO_PIN, 3000, 500);  
+  for (short i = 100; i < 10000; i++) {
+    tone(PIEZO_PIN, i, 100);  
+  }
 
   delay(1500);
 }
