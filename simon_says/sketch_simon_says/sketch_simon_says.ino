68432a22687e64dae7eed39fa1498cd9795b03a3
Armond Sarkisian <armond.sarkisian@gmail.com>
2023-11-09 17:06:01 -0800

    updated

--- simon_says/sketch_simon_says/sketch_simon_says.ino
+++ simon_says/sketch_simon_says/sketch_simon_says.ino
@@ -1,7 +1,10 @@
-#include <SPI.h>
-#include <Wire.h>
+
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
+#include <SPI.h>
+#include <Streaming.h>
+#include <Vector.h>
+#include <Wire.h>
 
 #define SERIAL_BAUD_RATE 9600
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
@@ -19,37 +22,26 @@
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 // button pins
-const int BUTTON_RED_PIN = A0;
-const int BUTTON_GREEN_PIN = A1;
-const int BUTTON_BLUE_PIN = A2;
-const int BUTTON_WHITE_PIN = A3;
-const int BUTTON_YELLOW_PIN = A4;
+uint8_t buttonPins[5] = {A0, A1, A2, A3, A4};
 
 // led pins
-const int LED_RED = 6;
-const int LED_GREEN = 5;
-const int LED_BLUE = 4;
-const int LED_WHITE = 3;
-const int LED_YELLOW = 2;
+int ledPins[5] = {2, 3, 4, 5, 6};
 
-void drawText(String text) {
-  display.clearDisplay();
+// buzzer pin
+const int PIEZO_BUZZER_PIN = 7;
 
-  display.setTextSize(1);              // Normal 1:1 pixel scale
-  display.setTextColor(SSD1306_WHITE); // Draw white text
-  display.setCursor(0,0);              // Start at top-left corner
-  display.println(text);
-
-  display.display();
-}
+// sequence
+const int SEQUENCE_COUNT_MAX = 30;
+typedef Vector<int> Sequence;
 
 void setup() {
   // engage serial communication
   Serial.begin(SERIAL_BAUD_RATE);
+  while (!Serial) { /* wait for serial port to connect */ }
 
   // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
   if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
-    Serial.println(F("SSD1306 OLED allocation failed"));
+    Serial << "Error: SSD1306 OLED Allocation Failed" << endl;
     for(;;); // Don't proceed, loop forever
   }
 
@@ -64,58 +56,82 @@ void setup() {
   display.clearDisplay();
 
   // set input pins (buttons)
-  pinMode(BUTTON_RED_PIN, INPUT);
-  pinMode(BUTTON_GREEN_PIN, INPUT);
-  pinMode(BUTTON_BLUE_PIN, INPUT);
-  pinMode(BUTTON_WHITE_PIN, INPUT);
-  pinMode(BUTTON_YELLOW_PIN, INPUT);
+  Vector<uint8_t> buttonPinsVec(buttonPins);
+  for (uint8_t button : buttonPinsVec) {
+    pinMode(button, INPUT);
+  }
 
   // set output pins (leds)
-  pinMode(LED_RED, OUTPUT);
-  pinMode(LED_GREEN, OUTPUT);
-  pinMode(LED_BLUE, OUTPUT);
-  pinMode(LED_WHITE, OUTPUT);
-  pinMode(LED_YELLOW, OUTPUT);      
+  Vector<int> ledPinsVec(ledPins);
+  for (int button : ledPinsVec) {
+    pinMode(button, INPUT);
+  }
+
+  // set output pins (buzzer)
+  //pinMode(PIEZO_BUZZER_PIN, OUTPUT);
+}
+
+void drawText(String text) {
+  display.clearDisplay();
+
+  display.setTextSize(1);              // Normal 1:1 pixel scale
+  display.setTextColor(SSD1306_WHITE); // Draw white text
+  display.setCursor(0,0);              // Start at top-left corner
+  display.println(text);
+
+  display.display();
+}
+
+Sequence generateSequence(int level) {
+  Sequence buffer;
+  int storageArray[SEQUENCE_COUNT_MAX];
+  buffer.setStorage(storageArray);
+
+  for (int i = 0; i < level; i++) {
+    const int RAN_NUM = random(2, 7);
+    buffer.push_back(RAN_NUM);
+  }
+
+  Serial << "Buffer ( in the function ): " << buffer << endl;
+
+  return buffer;
+}
+
+Sequence activateSequence(Sequence seq) {
+  Serial << "Sequence ( in the function ): " << seq << endl;
+  for (int d : seq) {
+    digitalWrite(d, HIGH);
+    delay(750);
+    digitalWrite(d, LOW);
+  }
+
+  Serial << "Sequence ( inside the FUNCTION ): " << seq << endl;
+
+  return seq;
 }
 
 void loop() {
-  int buttonRed = digitalRead(BUTTON_RED_PIN);
-  int buttonGreen = digitalRead(BUTTON_GREEN_PIN);
-  int buttonBlue = digitalRead(BUTTON_BLUE_PIN);
-  int buttonWhite = digitalRead(BUTTON_WHITE_PIN);
-  int buttonYellow = digitalRead(BUTTON_YELLOW_PIN);
+  static int level = 1;
 
-  if (buttonRed == HIGH) {
-    digitalWrite(LED_RED, HIGH);
-  } else {
-    digitalWrite(LED_RED, LOW);
-  }
+  // display the level number
+  drawText("Level: " + String(level));
 
-  if (buttonGreen == HIGH) {
-    digitalWrite(LED_GREEN, HIGH);
-  } else {
-    digitalWrite(LED_GREEN, LOW);
-  }
+  // display the light sequence + play sounds
+  Sequence seq = activateSequence(generateSequence(level));
 
-  if (buttonBlue == HIGH) {
-    digitalWrite(LED_BLUE, HIGH);
-  } else {
-    digitalWrite(LED_BLUE, LOW);
-  }
+  Serial << "Sequence: (outside the FUNCTION) " << seq << endl;
 
-  if (buttonWhite == HIGH) {
-    digitalWrite(LED_WHITE, HIGH);
-  } else {
-    digitalWrite(LED_WHITE, LOW);
-  }
+  // get input
+  //int buttonRed = digitalRead(BUTTON_RED_PIN);
+  //int buttonGreen = digitalRead(BUTTON_GREEN_PIN);
+  //int buttonBlue = digitalRead(BUTTON_BLUE_PIN);
+  //int buttonWhite = digitalRead(BUTTON_WHITE_PIN);
+  //int buttonYellow = digitalRead(BUTTON_YELLOW_PIN);
 
-  if (buttonYellow == HIGH) {
-    digitalWrite(LED_YELLOW, HIGH);
-  } else {
-    digitalWrite(LED_YELLOW, LOW);
-  }
+  // determine if correct
 
-  drawText("Hello Armond");
+  // if correct --> go to next level
+  // if not correct, display "Game Over" and restart back to level 1
 
-  delay(100);
+  delay(1000);
 }
